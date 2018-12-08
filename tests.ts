9acc8fb1ad3ae6a66ff8d60893e8faa4f7f3bfc5
bluesanta <bluesanta@DESKTOP-CA82AM5>
2018-12-08 09:54:43 +0900

    아이콘변경, 라이센스변경, 테스트스크립트 변경

--- tests.ts
+++ tests.ts
@@ -1,9 +1,9 @@
 // tests go here; this will not be compiled when this package is used as a library
 
-ultrasonic.init(Pins.P0, Pins.P1)
-basic.forever(() => {
-    ultrasonic.start()
-    basic.pause(500)
-    basic.showNumber(ultrasonic.get_dist_cm())
-    basic.pause(500)
+let 거리 = 0
+Ultrasonic.init(Pins.P1, Pins.P2)
+basic.forever(function () {
+    거리 = Ultrasonic.get_dist_cm()
+    basic.showNumber(거리)
+    basic.pause(1000)
 })
